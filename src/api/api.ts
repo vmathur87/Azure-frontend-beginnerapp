7cf075032afdad180bcb4a35f72e8e47b0ca34fd
vmathur87 <vmathur87@gmail.com>
2024-06-16 14:12:56 +0530

    Update api.ts to access API_ENDPOINT

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import { ChatAppResponse, ChatAppResponseOrError, ChatAppRequest, Config } from "./models";
 import { BACKEND_URI } from "./BACKEND_URI";
+const API_ENDPOINT = process.env.API_ENDPOINT;
 
 function getHeaders(): Record<string, string> {
     var headers: Record<string, string> = {
@@ -10,7 +11,7 @@ function getHeaders(): Record<string, string> {
 
 export async function chatApi(request: ChatAppRequest): Promise<Response> {
     const body = JSON.stringify(request);
-    return await fetch(`${BACKEND_URI}/ai`, {
+    return await fetch(`${API_ENDPOINT}/ai`, {
         method: "POST",
         mode: "cors",
         headers: getHeaders(),
